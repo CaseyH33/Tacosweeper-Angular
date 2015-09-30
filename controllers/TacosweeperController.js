9ae958f00f25f32ec1c4a94eee3672eb71d6186e
Ben Casalino & Casey Heitz & Annie Hoogendoorn <student@epicodus.com>
2015-09-30 15:17:47 -0700

    Add bombcount number to spots

--- controllers/TacosweeperController.js
+++ controllers/TacosweeperController.js
@@ -5,6 +5,7 @@ tacosweeper.controller('TacosweeperCtrl', function TacosweeperCtrl($scope) {
     var tacofield= {};
     tacofield.rows = [];
 
+    //Add ability to change board size
     for(var i=0; i<9; i++) {
       var row = {};
       row.spots = [];
@@ -19,8 +20,8 @@ tacosweeper.controller('TacosweeperCtrl', function TacosweeperCtrl($scope) {
       tacofield.rows.push(row);
     }
 
-    placeRandomBomb(tacofield);
-
+    placeManyRandomBombs(tacofield, 10);
+    calculateAllNumbers(tacofield);
     return tacofield;
   };
 
@@ -32,8 +33,98 @@ tacosweeper.controller('TacosweeperCtrl', function TacosweeperCtrl($scope) {
     var row = Math.round(Math.random() * 8);
     var column = Math.round(Math.random() * 8);
     var spot = getSpot(tacofield, row, column);
+    //Check if spot already is a bomb
     spot.content ="bomb";
   };
 
+  function placeManyRandomBombs(tacofield, bombNum) {
+    for(var i =0; i<bombNum; i++) {
+      placeRandomBomb(tacofield);
+    };
+  };
 
+  function calculateNumber(tacofield, row, column) {
+    var thisSpot = getSpot(tacofield, row, column);
+
+    //if this spot contains a mine then we can't place a number here
+    if (thisSpot.content == "bomb") {
+      return;
+    }
+
+    var bombCount = 0;
+
+    //check row above if this is not the first row
+    if(row > 0) {
+      //check column to the left if this is not the first column
+      if(column > 0) {
+        //get the spot above and to the left
+        var spot = getSpot(tacofield, row - 1, column - 1);
+        if(spot.content == "bomb") {
+          bombCount++;
+        }
+      }
+
+      //get the spot right above
+      var spot = getSpot(tacofield, row -1, column);
+      if(spot.content == "bomb") {
+        bombCount++;
+      }
+
+      //check column to the right if this is not the last column
+      if(column < 8) {
+        //get the spot above and to the right
+        var spot = getSpot(tacofield, row - 1, column + 1);
+        if(spot.content == "bomb") {
+          bombCount++;
+        }
+      }
+
+    }
+    // check column to the left if this is no the first column
+    if (column < 8 ) {
+      // get the spot to the right
+      var spot = getSpot(tacofield, row, column +1);
+      if (spot.content =="bomb") {
+        bombCount++;
+      }
+  }
+
+    // check row below if it is not the last row
+    if (row < 8 ) {
+     // check column to the left if this is not the first column
+      if (column > 0) {
+        // get the spot to the left
+        var spot = getSpot(tacofield, row +1, column -1);
+        if (spot.content =="bomb") {
+          bombCount++;
+        }
+      }
+       // get the spot right below
+     var spot = getSpot(tacofield, row +1, column);
+     if (spot.content =="bomb") {
+       bombCount++;
+      }
+
+     // check column to the right if it is not the last column
+     if (column < 8 ) {
+     // get the spot below and to the right
+       var spot = getSpot(tacofield, row +1, column +1);
+       if (spot.content =="bomb") {
+         bombCount++;
+         }
+      }
+    }
+
+  if (bombCount > 0 ) {
+    thisSpot.content = bombCount;
+  }
+  }
+
+  function calculateAllNumbers(tacofield) {
+    for (var y = 0; y < 9; y++ ) {
+        for(var x = 0; x < 9; x++) {
+            calculateNumber(tacofield, x, y);
+        }
+    }
+  }
 });
